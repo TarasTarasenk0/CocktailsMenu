c5b5d9958997c59c02b425bc8b741b6309ce3f84
Taras Tarasenko <myavailability.taras@gmail.com>
2020-06-18 20:46:55 +0300

    Created models

---
+++ CocktailsMenu/Models/Categories.swift
@@ -0,0 +1,13 @@
+//
+//  Categories.swift
+//  CocktailsMenu
+//
+//  Created by md760 on 18.06.2020.
+//  Copyright © 2020 md760. All rights reserved.
+//
+
+import Foundation
+
+struct Categories: Codable {
+    let strCategory: String
+}
---
+++ CocktailsMenu/Models/Drink.swift
@@ -0,0 +1,15 @@
+//
+//  Drink.swift
+//  CocktailsMenu
+//
+//  Created by md760 on 18.06.2020.
+//  Copyright © 2020 md760. All rights reserved.
+//
+
+import Foundation
+
+struct Drink: Codable {
+    let idDrink: String
+    let strDrink: String
+    let strDrinkThumb: String
+}
